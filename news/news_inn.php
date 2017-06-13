dbcf69f2c83b2a8e123b9826b0b68f049289342a
Rilind Pozhegu <Rilind Pozhegu>
2017-06-13 13:41:33 +0200

    news innerpage

---
+++ news/news_inn.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<!-- Tab Logo -->
+    <link rel="icon" href="img/fav_icon-01.png">
+
+    <title>Emirates Landscape</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../build/styles.css">
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<style type="text/css">
+    .active6 {
+          background-color: #C93851 !important;
+          color: white !important;
+          transition: ease-in-out all 0.2s;
+    }
+</style>
+
+
+   <!-- Navigation -->
+    <?php include '../navigation_bar_innerpages.php';?>
+
+
+    <!-- Slider -->
+    <?php include '../header_innerpages.php';?>
+
+
+    <!-- Main text Content of News innerpage -->
+
+    <section class="main_news_innerpage_s">
+      <div class="container">
+        <div class="main_news_innerpage_s1">
+          <div class="row">
+            <div class="col-md-5 no_padding">
+              <img src="../img/construction_img31-01.png" class="img-responsive">
+            </div>
+            <div class="col-md-7">
+              <h2>2 million hours <br> without any lost-time accident!</h2>
+              <p>As subcontractor to Serco we have achieved ”2 million hours without any lost-time accident” at Paris Sorbonne University.</p>
+              <p>As subcontractor to Serco we have achieved ”2 million hours without any lost-time accident” at Paris Sorbonne University.</p>
+            </div>
+            <div class="clearfix"></div>
+              <p class="text_insidenews_in">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <p class="text_insidenews_in">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>  
+        </div>
+      </div>
+    </section>
+
+
+
+    <!-- Contact Inputs section on all the pages + footer -->
+    <?php include '../contact_footer_all.php';?>
+    <!-- jQuery -->
+    <script type="text/javascript" src="../build/scripts.js"></script>
+
+</body>
+
+</html>
